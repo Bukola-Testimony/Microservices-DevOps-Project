5c862c198cbf0b53a8d4035a5d01eb2aa2bcd1d4
Bukola Testimony <Bukolatestimony2@gmail.com>
2023-03-08 20:59:43 +0100

    Update provider.tf

--- Terraform-aws-eks/provider.tf
+++ Terraform-aws-eks/provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"
+  #profile = "default"
+  access_key = "credentials('AWS_ACCESS_KEY_ID')"
+  secret_key = "credentials('AWS_SECRET_ACCESS_KEY')"   
 }
 
